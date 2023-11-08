87d2bcdb31c7d970105127b8ca20473725d9c00a
joseph-czinege <105775825+joseph-czinege@users.noreply.github.com>
2023-11-08 10:05:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Clearabee Tech Test
+
+## Instructions
+
+Thank you for you interest in the developer role at Clearabee. For this task, please follow the instructions below:
+
+- Create a private repository in your **own** github account using this **[template](https://github.com/Clearabee/Tech-Test)**
+- Create a new branch off the main branch
+- Commit your work on the new branch, and push it to github
+- Invite Naveed Ziarab, Head of Engineering, to collaborate on the project **[@Nav33d](https://github.com/Nav33d)**
+- Create a pull request against the main branch and add Naveed as a reviewer
+
+## Test
+
+We ask that you complete the function _getDeliveryOptions_ in deliveryOptions.ts, so all tests pass in deliveryOptions.test.ts.
+
+## Context
+
+The test is based on a real situation we had to solve at Clearabee: we have a number of products with different delivery options. If a customer orders, for example, two products, then we must find matching delivery options for those products, so they can be delivered at the same time.
+
+## Time
+
+We know how time consuming tech tests can be, so we have intentionally designed this not to take more than a few hours. If you only complete part of the test, please submit what you have done, as **we are as interested in how you go about the task as we are in you completing it**.
+
+## Install
+
+`npm i`
+
+## Tests
+
+This runs the tests in watch mode, so they will run again any time a change is saved to deliveryOptions.ts.
+
+`npm run test`
+
+If you have any questions or issues, please do not hesitate to reach out to Naveed Ziarab at naveed.ziarab@clearabee.co.uk.
+
+We look forward to discussing your submission with you.
---
+++ deliveryOptions.test.ts
@@ -0,0 +1,193 @@
+import { getDeliveryOptions } from "./deliveryOptions";
+
+describe("getDeliveryOptions", () => {
+  test("returns all delivery options for a single product", () => {
+    const deliveryOptions = [
+      {
+        productName: "Product 1",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 1",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 20,
+          },
+          {
+            name: "Delivery Option 2",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 50,
+          },
+        ],
+      },
+    ];
+
+    const expected = [
+      {
+        name: "Delivery Option 1",
+        timeSlot: { from: "10:00", to: "12:00" },
+        price: 20,
+      },
+      {
+        name: "Delivery Option 2",
+        timeSlot: { from: "12:00", to: "13:00" },
+        price: 50,
+      },
+    ];
+
+    expect(getDeliveryOptions(deliveryOptions)).toEqual(expected);
+  });
+
+  test("returns the first matching delivery option for multiple products with matching timeslots", () => {
+    const deliveryOptions = [
+      {
+        productName: "Product 1",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 1",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 20,
+          },
+          {
+            name: "Delivery Option 2",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+        ],
+      },
+      {
+        productName: "Product 2",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 3",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 20,
+          },
+          {
+            name: "Delivery Option 4",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+        ],
+      },
+    ];
+
+    const expected = [
+      {
+        name: "Delivery Option 1",
+        timeSlot: { from: "10:00", to: "12:00" },
+        price: 20,
+      },
+      {
+        name: "Delivery Option 2",
+        timeSlot: { from: "12:00", to: "13:00" },
+        price: 30,
+      },
+    ];
+
+    expect(getDeliveryOptions(deliveryOptions)).toEqual(expected);
+  });
+
+  test("returns the highest priced delivery options for multiple products with matching timeslots", () => {
+    const deliveryOptions = [
+      {
+        productName: "Product 1",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 1",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 20,
+          },
+          {
+            name: "Delivery Option 2",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+        ],
+      },
+      {
+        productName: "Product 2",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 3",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 50,
+          },
+          {
+            name: "Delivery Option 4",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+        ],
+      },
+    ];
+
+    const expected = [
+      {
+        name: "Delivery Option 3",
+        timeSlot: { from: "10:00", to: "12:00" },
+        price: 50,
+      },
+      {
+        name: "Delivery Option 2",
+        timeSlot: { from: "12:00", to: "13:00" },
+        price: 30,
+      },
+    ];
+
+    expect(getDeliveryOptions(deliveryOptions)).toEqual(expected);
+  });
+
+  test("returns the highest priced delivery options for multiple products with matching timeslots only", () => {
+    const deliveryOptions = [
+      {
+        productName: "Product 1",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 1",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 20,
+          },
+          {
+            name: "Delivery Option 2",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+        ],
+      },
+      {
+        productName: "Product 2",
+        deliveryOptions: [
+          {
+            name: "Delivery Option 3",
+            timeSlot: { from: "10:00", to: "12:00" },
+            price: 50,
+          },
+          {
+            name: "Delivery Option 4",
+            timeSlot: { from: "12:00", to: "13:00" },
+            price: 30,
+          },
+          {
+            name: "Delivery Option 5",
+            timeSlot: { from: "14:00", to: "15:00" },
+            price: 30,
+          },
+        ],
+      },
+    ];
+
+    const expected = [
+      {
+        name: "Delivery Option 3",
+        timeSlot: { from: "10:00", to: "12:00" },
+        price: 50,
+      },
+      {
+        name: "Delivery Option 2",
+        timeSlot: { from: "12:00", to: "13:00" },
+        price: 30,
+      },
+    ];
+
+    expect(getDeliveryOptions(deliveryOptions)).toEqual(expected);
+  });
+});
